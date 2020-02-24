a8a326146ea345c856c096c603755b839d506715
Cramer Grimes <cramergrimes@Cramers-MBP.localdomain>
2020-02-24 10:44:20 -0500

    create basic file structure

---
+++ reactApp.js
@@ -0,0 +1,12 @@
+
+function App(props){
+    return(
+        <div>
+            <h1>Welcome to the starte Sports Game</h1>
+            we are going to have a lot of fun
+        </div>
+    );
+}
+
+
+ReactDOM.render(<App/>,document.getElementById('root'));
